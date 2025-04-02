aba1392a953544896e291e8de33ab231c63182bd
sawi2003 <96415110+sawi2003@users.noreply.github.com>
2025-04-02 12:28:01 +0530

    Update config.js

--- config.js
+++ config.js
@@ -8,8 +8,8 @@ function convertToBool(text, fault = 'true') {
 module.exports = {
 
 SESSION_ID: process.env.SESSION_ID === undefined ? '6lIS2LwB#YGg8JKKc7C1sppIpvLrr2J66tmdEQnj5tYnA3yNIFaI' : process.env.SESSION_ID, 
-GITHUB_AUTH_TOKEN: process.env.GITHUB_AUTH_TOKEN === undefined ? 'put github auth token' : process.env.GITHUB_AUTH_TOKEN,  //🛑 Github token එක දැමීමේදී ghp_  කොටස ඉවර් කර දමන්න.  
-GITHUB_USER_NAME: process.env.GITHUB_USER_NAME === undefined ? 'Github user name' : process.env.GITHUB_USER_NAME, // 🛑 Github user name එක ගද්දි ඔයාලා token එක ගත්ත Github එකේ user name එක දාන්න අනිවාරෙන්
+GITHUB_AUTH_TOKEN: process.env.GITHUB_AUTH_TOKEN === undefined ? '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' : process.env.GITHUB_AUTH_TOKEN,  //🛑 Github token එක දැමීමේදී ghp_  කොටස ඉවර් කර දමන්න.  
+GITHUB_USER_NAME: process.env.GITHUB_USER_NAME === undefined ? 'sawi2003' : process.env.GITHUB_USER_NAME, // 🛑 Github user name එක ගද්දි ඔයාලා token එක ගත්ත Github එකේ user name එක දාන්න අනිවාරෙන්
 PORT: process.env.PORT === undefined ? '8000' : process.env.PORT, 
 
 
